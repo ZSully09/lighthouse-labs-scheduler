1aab793c764439ae5ea6d9d434954ed1aa0ebe7c
Zach Sullivan <zsullivan93@gmail.com>
2019-11-26 13:19:58 -0800

    updated stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment';
 import Header from 'components/Appointment';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf('Button', module)
   .addParameters({
@@ -140,5 +141,17 @@ storiesOf('Appointment', module)
   .add('Header', () => <Header time="12pm" />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
   .add('Show', () => (
-    <Show onEdit={action('edit')} onDelete={action('delete')} />
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewers[0]}
+      onEdit={action('edit')}
+      onDelete={action('delete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('confirm')}
+      onDelete={action('delete')}
+    />
   ));
