9af7f87932b2414b9c1686277f1bc5694cdcd031
Zach Sullivan <zsullivan93@gmail.com>
2019-11-27 13:52:41 -0800

    import Show and Empty; updated return to render components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  return <article className="appointment">{props.time}</article>;
+  return (
+    <article className="appointment">
+      <header>
+        {props.time}
+        {props.interview ? (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+            onEdit={props.onEdit}
+            onDelete={props.onDelete}
+          />
+        ) : (
+          <Empty onAdd={props.onAdd} />
+        )}
+      </header>
+    </article>
+  );
 }
