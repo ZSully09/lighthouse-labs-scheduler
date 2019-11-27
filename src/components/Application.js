bb8c6ebb1a63382bd5b9983d2ebbcd1be9b296b8
Zach Sullivan <zsullivan93@gmail.com>
2019-11-27 13:51:44 -0800

    imports for useState, useEffect, axios, and Appointment; removed hardcoded days data to make axios request; added hardcoded mock appointment data; mapped appointments to pass Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import DayList from 'components/DayList';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment';
 import 'components/Application.scss';
 
-const days = [
+const appointments = [
   {
     id: 1,
-    name: 'Monday',
-    spots: 2
+    time: '12pm'
   },
   {
     id: 2,
-    name: 'Tuesday',
-    spots: 5
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png'
+      }
+    }
   },
   {
     id: 3,
-    name: 'Wednesday',
-    spots: 0
+    time: '2pm',
+    interview: {
+      student: 'Zach',
+      interviewer: {
+        id: 2,
+        name: 'Tori Malcolm',
+        avatar: 'https://i.imgur.com/Nmx0Qxo.png'
+      }
+    }
+  },
+  {
+    id: 4,
+    time: '3pm'
+  },
+  {
+    id: 5,
+    time: '4pm',
+    interview: {
+      student: 'James',
+      interviewer: {
+        id: 3,
+        name: 'Mildred Nazir',
+        avatar: 'https://i.imgur.com/T2WwVfS.png'
+      }
+    }
   }
 ];
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/days')
+      .then(response => {
+        setDays(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        console.log(error.response.data);
+      });
+  }, []);
+
+  const apptList = appointments.map(appointment => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
 
   const SelectDay = () => {
     setDay(day);
@@ -47,7 +97,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {apptList}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
