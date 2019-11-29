179af485976ffa955a540a4be9cef7c9387514d0
Zach Sullivan <zsullivan93@gmail.com>
2019-11-29 15:16:37 -0800

    useVisualMode hook and tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(mode, replace = false) {
+    if (replace === true) {
+      setMode(mode);
+    } else {
+      setMode(mode);
+      setHistory([...history, mode]);
+      // console.log([...history, mode]);
+    }
+  }
+
+  function back() {
+    if (history.length === 1) {
+      setMode(initial);
+    } else {
+      setMode(history[history.length - 2]);
+      setHistory(history.slice(0, -1));
+    }
+  }
+
+  return { mode, transition, back };
+}
