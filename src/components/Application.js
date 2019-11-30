04270f704846dc51db33414d90a0f61c5eb2f308
Zach Sullivan <zsullivan93@gmail.com>
2019-11-30 15:40:16 -0800

    refactoring for useApplicationData

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,114 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
+import useApplicationData from 'hooks/useApplicationData';
 import DayList from 'components/DayList';
 // import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
-import { getAppointmentsForDay, getInterview } from 'helpers/selectors.js';
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay
+} from 'helpers/selectors.js';
 import 'components/Application.scss';
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: '12pm'
-//   },
-//   {
-//     id: 2,
-//     time: '1pm',
-//     interview: {
-//       student: 'Lydia Miller-Jones',
-//       interviewer: {
-//         id: 1,
-//         name: 'Sylvia Palmer',
-//         avatar: 'https://i.imgur.com/LpaY82x.png'
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: '2pm',
-//     interview: {
-//       student: 'Zach',
-//       interviewer: {
-//         id: 2,
-//         name: 'Tori Malcolm',
-//         avatar: 'https://i.imgur.com/Nmx0Qxo.png'
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: '3pm'
-//   },
-//   {
-//     id: 5,
-//     time: '4pm',
-//     interview: {
-//       student: 'James',
-//       interviewer: {
-//         id: 3,
-//         name: 'Mildred Nazir',
-//         avatar: 'https://i.imgur.com/T2WwVfS.png'
-//       }
-//     }
-//   }
-// ];
+export default function Application() {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
+  // const [state, setState] = useState({
+  //   day: 'Monday',
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {}
+  // });
 
-export default function Application(props) {
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => setState({ ...state, day });
-  // const setDays = days => setState(prev => ({ ...prev, days }));
-
-  // const [day, setDay] = useState('Monday');
-  // const [days, setDays] = useState([]);
-
-  // useEffect(() => {
-  //   axios.get('/api/days').then(response => {
-  //     setDays(response.data);
-  //   });
-  // }, []);
+  // const setDay = day => setState({ ...state, day });
 
   // useEffect(() => {
   //   Promise.all([
   //     axios.get('/api/days'),
-  //     axios.get('/api/appointments')
-  //     // axios.get('/api/interviewers')
-  //   ]).then(all => {
-  //     console.log(all);
+  //     axios.get('/api/appointments'),
+  //     axios.get('/api/interviewers')
+  //   ]).then(response => {
+  //     setState(prev => ({
+  //       ...prev,
+  //       days: response[0].data,
+  //       appointments: response[1].data,
+  //       interviewers: response[2].data
+  //     }));
   //   });
   // }, []);
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(response => {
-      setState(prev => ({
-        ...prev,
-        days: response[0].data,
-        appointments: response[1].data,
-        interviewers: response[2].data
-      }));
-    });
-  }, []);
   // console.log(state.interviewers);
 
-  const appointments = getAppointmentsForDay(state, state.day);
+  // function bookInterview(id, interview) {
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: { ...interview }
+  //   };
 
-  const schedule = appointments.map(appointment => {
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
+
+  //   setState(state => ({
+  //     ...state,
+  //     appointments
+  //   }));
+  //   return axios.put(`/api/appointments/${id}`, { interview });
+  // }
+
+  // function cancelInterview(id, interview) {
+  //   return axios.delete(`/api/appointments/${id}`, { interview }).then(() => {
+  //     const appointment = {
+  //       ...state.appointments[id],
+  //       interview: { ...interview }
+  //     };
+
+  //     const appointments = {
+  //       ...state.appointments,
+  //       [id]: appointment
+  //     };
+
+  //     setState(state => ({
+  //       ...state,
+  //       appointments
+  //     }));
+  //   });
+  // }
+
+  // const appointments = getAppointmentsForDay(state, state.day);
+  // const interviewers = getInterviewersForDay(state, state.day);
+  const schedule = getAppointmentsForDay(state, state.day).map(appointment => {
     const interview = getInterview(state, appointment.interview);
+    const interviewers = getInterviewersForDay(state, state.day);
+    // const schedule = appointments.map(appointment => {
+    // const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
-        interview={interview}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
