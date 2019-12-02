e168d0032fd8ff72d3b9c2f6f1c351b032823ed7
Zach Sullivan <zsullivan93@gmail.com>
2019-12-01 17:05:59 -0800

    removed unused code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,14 @@
-// export function getAppointmentsForDay(state, day) {
-//   const filteredAppointments = state.days.filter(days => days.name === day);
-//   let appointments = [];
-//   if (filteredAppointments.length) {
-//     appointments = filteredAppointments[0].appointments.map(
-//       d => state.appointments[d]
-//     );
-//   }
-//   return appointments;
-// }
-
 export function getAppointmentsForDay(state, day) {
+  //   const filteredAppointments = state.days.filter(a => a.name === day);
+  //   let appointments = [];
+  //   if (filteredAppointments.length) {
+  //     appointments = filteredAppointments[0].appointments.map(
+  //       b => state.appointments[b]
+  //     );
+  //   }
+  //   return appointments;
+  // }
   const validDays = state.days.map(day => day.name);
-  // console.log('validDays', validDays);
   if (!day || !validDays.includes(day)) return [];
 
   return state.days
@@ -40,7 +37,7 @@ export function getInterviewersForDay(state, day) {
   // }
 
   const filteredInterviewers = state.days.filter(days => days.name === day);
-  // console.log(filteredInterviewers);
+
   let interviewers = [];
   if (filteredInterviewers.length && filteredInterviewers[0].appointments) {
     interviewers = filteredInterviewers[0].interviewers.map(
@@ -49,45 +46,3 @@ export function getInterviewersForDay(state, day) {
   }
   return interviewers;
 }
-
-// let results = [];
-// for (let key in state.appointments) {
-//   if ()
-// }
-
-// const filtered = state.appointments[filteredAppointmentIds];
-// // const filtered = filteredDays.appointments.map(id => {
-// //   appointments[id];
-// // });
-// console.log(filteredAppointmentIds);
-// return filteredAppointmentIds;
-
-// const state = {
-//   days: [
-//     {
-//       id: 1,
-//       name: 'Monday',
-//       appointments: [1, 2, 3]
-//     },
-//     {
-//       id: 2,
-//       name: 'Tuesday',
-//       appointments: [4, 5]
-//     }
-//   ],
-//   appointments: {
-//     '1': { id: 1, time: '12pm', interview: null },
-//     '2': { id: 2, time: '1pm', interview: null },
-//     '3': {
-//       id: 3,
-//       time: '2pm',
-//       interview: { student: 'Archie Cohen', interviewer: 2 }
-//     },
-//     '4': { id: 4, time: '3pm', interview: null },
-//     '5': {
-//       id: 5,
-//       time: '4pm',
-//       interview: { student: 'Chad Takahashi', interviewer: 2 }
-//     }
-//   }
-// };
