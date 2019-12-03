8a9463fb459617134cc6e89567d866959122df8c
Zach Sullivan <zsullivan93@gmail.com>
2019-12-03 12:21:15 -0800

    test refactoring to use fixture data

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, waitForElement } from '@testing-library/react';
 
 import Application from 'components/Application';
+import { fireEvent } from '@testing-library/react/dist';
+import { exportAllDeclaration } from '@babel/types';
 
 afterEach(cleanup);
 
-it('renders without crashing', () => {
-  render(<Application />);
+describe('Application', () => {
+  // it('renders without crashing', () => {
+  //   render(<Application />);
+  it('changes the schedule when a new day is selected', async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText('Monday'));
+
+    fireEvent.click(getByText('Tuesday'));
+
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
 });
