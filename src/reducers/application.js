495983d2a592ce9cb9e6d7f5f0cc33deda8c61a3
Zach Sullivan <zsullivan93@gmail.com>
2019-12-04 10:25:05 -0800

    implemented reducer

---
+++ src/reducers/application.js
@@ -0,0 +1,60 @@
+export const SET_DAY = 'SET_DAY';
+export const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
+export const SET_INTERVIEW = 'SET_INTERVIEW';
+
+function getSpotsRemainingForDay(day, appointments) {
+  let spotsForThisDay = day.appointments;
+  let freeSpots = 0;
+  for (const spot of spotsForThisDay) {
+    if (appointments[spot].interview === null) {
+      freeSpots++;
+    }
+  }
+  return freeSpots;
+}
+
+function decorateDaysWithSpots(days, appointments) {
+  const decoratedDays = days.map(day => ({
+    ...day,
+    spots: getSpotsRemainingForDay(day, appointments)
+  }));
+  return decoratedDays;
+}
+
+export default function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY:
+      return {
+        ...state,
+        day: action.day
+      };
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      };
+    case SET_INTERVIEW: {
+      const appointments = {
+        ...state.appointments,
+        [action.id]: {
+          ...state.appointments[action.id],
+          interview: action.interview === null ? null : { ...action.interview }
+        }
+      };
+      const days = decorateDaysWithSpots(state.days, appointments);
+
+      return {
+        ...state,
+        days,
+        appointments
+      };
+    }
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
