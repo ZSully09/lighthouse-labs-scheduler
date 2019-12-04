87df233a07a536f13738ee283daf0647b45af148
Zach Sullivan <zsullivan93@gmail.com>
2019-12-03 16:00:03 -0800

    put and delete mock functions

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -23,6 +23,20 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+
+  put: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: 'No Content'
+    });
+  }),
+
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: 'No Content'
+    });
   })
 };
 
