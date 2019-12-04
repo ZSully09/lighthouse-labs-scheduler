01317a4547fcb392db864bc43185862b2cae8710
Zach Sullivan <zsullivan93@gmail.com>
2019-12-04 10:21:20 -0800

    moved reducer into reducer/application

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,66 +1,70 @@
 import { useReducer, useEffect } from 'react';
 import axios from 'axios';
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from 'reducers/application';
+// const SET_DAY = 'SET_DAY';
+// const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
+// const SET_INTERVIEW = 'SET_INTERVIEW';
 
-const SET_DAY = 'SET_DAY';
-const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
-const SET_INTERVIEW = 'SET_INTERVIEW';
+// function getSpotsRemainingForDay(day, appointments) {
+//   let spotsForThisDay = day.appointments;
+//   let freeSpots = 0;
+//   for (const spot of spotsForThisDay) {
+//     if (appointments[spot].interview === null) {
+//       freeSpots++;
+//     }
+//   }
+//   return freeSpots;
+// }
 
-function getSpotsRemainingForDay(day, appointments) {
-  let spotsForThisDay = day.appointments;
-  let freeSpots = 0;
-  for (const spot of spotsForThisDay) {
-    if (appointments[spot].interview === null) {
-      freeSpots++;
-    }
-  }
-  return freeSpots;
-}
+// function decorateDaysWithSpots(days, appointments) {
+//   const decoratedDays = days.map(day => ({
+//     ...day,
+//     spots: getSpotsRemainingForDay(day, appointments)
+//   }));
+//   return decoratedDays;
+// }
 
-function decorateDaysWithSpots(days, appointments) {
-  const decoratedDays = days.map(day => ({
-    ...day,
-    spots: getSpotsRemainingForDay(day, appointments)
-  }));
-  return decoratedDays;
-}
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case SET_DAY:
+//       return {
+//         ...state,
+//         day: action.day
+//       };
+//     case SET_APPLICATION_DATA:
+//       return {
+//         ...state,
+//         days: action.days,
+//         appointments: action.appointments,
+//         interviewers: action.interviewers
+//       };
+//     case SET_INTERVIEW: {
+//       const appointments = {
+//         ...state.appointments,
+//         [action.id]: {
+//           ...state.appointments[action.id],
+//           interview: action.interview === null ? null : { ...action.interview }
+//         }
+//       };
+//       const days = decorateDaysWithSpots(state.days, appointments);
 
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return {
-        ...state,
-        day: action.day
-      };
-    case SET_APPLICATION_DATA:
-      return {
-        ...state,
-        days: action.days,
-        appointments: action.appointments,
-        interviewers: action.interviewers
-      };
-    case SET_INTERVIEW: {
-      const appointments = {
-        ...state.appointments,
-        [action.id]: {
-          ...state.appointments[action.id],
-          interview: action.interview === null ? null : { ...action.interview }
-        }
-      };
-      const days = decorateDaysWithSpots(state.days, appointments);
+//       return {
+//         ...state,
+//         days,
+//         appointments
+//       };
+//     }
 
-      return {
-        ...state,
-        days,
-        appointments
-      };
-    }
-
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
+//     default:
+//       throw new Error(
+//         `Tried to reduce with unsupported action type: ${action.type}`
+//       );
+//   }
+// }
 
 export default function useApplicationData() {
   function bookInterview(id, interview) {
