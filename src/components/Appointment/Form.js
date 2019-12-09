0f2e39f7d7bb1306cfa98bf3893430a7f05f723a
Zach Sullivan <zsullivan93@gmail.com>
2019-12-08 18:32:54 -0800

    interviewer validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -27,6 +27,10 @@ export default function Form(props) {
       setError('Student name cannot be blank');
       return;
     }
+    if (!interviewer) {
+      setError('Please select an interviewer');
+      return;
+    }
 
     setError('');
     props.onSave(name, interviewer);
